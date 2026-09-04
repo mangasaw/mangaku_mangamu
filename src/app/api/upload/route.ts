2d4be92a9c37a88c62fed77e13bdae931bab3ca8
vicktorindra <xyz.mangasaw@gmail.com>
2026-09-04 11:43:45 +0700

    fix error

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -45,9 +45,3 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: 'Failed to upload file' }, { status: 500 })
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
